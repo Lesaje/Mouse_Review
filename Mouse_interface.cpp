0ef6fef3a2b5db016e59a6b084ecdb09fbf9b934
Oleg Chaika <59537764+Lesaje@users.noreply.github.com>
2021-10-15 21:11:48 +0300

    Update Mouse_interface.cpp

--- Mouse_interface.cpp
+++ Mouse_interface.cpp
@@ -6,28 +6,29 @@
 const int64_t MOD = 1000000007;
 
 /*
-Integer values not exceeding 1 000 000 007
+Matrix multiplication with integer values not exceeding 1 000 000 007
 */
-void matrix_multiplication(std::vector<int64_t>* first_matrix,
-    std::vector<int64_t>* second_matrix, std::vector<int64_t>* result_matrix)
-
-
-/*
-Raises a adjacency_matrix to a power, saving result in result_matrix
-*/
-void binpow(int64_t power, std::vector<int64_t>* adjacency_matrix,
+void matrix_multiplication(const std::vector<int64_t>& first_matrix,
+    const std::vector<int64_t>& second_matrix,
     std::vector<int64_t>* result_matrix)
-
+    
     
 std::vector<int64_t> make_identity_matrix(const int64_t matrix_size)
-
     
+
+/*
+Raises adjacency_matrix to a power
+*/
+std::vector<int64_t> binpow(int64_t power,
+    const std::vector<int64_t>& adjacency_matrix)
+    
+
 std::vector<int64_t> make_adjacency_matrix(const int64_t number_of_edges,
     const int64_t matrix_size, std::istream& in)
+    
 
-    
 std::tuple<int64_t, int64_t, int64_t> read(std::istream& in)
-    
+
     
 int64_t get_answer(const std::vector<int64_t>& result_matrix)
 
